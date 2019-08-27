db0e23d136fa3b59830c4858808d4971996a77e6
jalvarezufro <48539136+jalvarezufro@users.noreply.github.com>
2019-08-27 00:43:12 -0400

    Initial commit

---
+++ src/main/java/proyecto/atmosfera/ProyectoAtmosferaApplication.java
@@ -0,0 +1,13 @@
+package proyecto.atmosfera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAtmosferaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoAtmosferaApplication.class, args);
+    }
+
+}
