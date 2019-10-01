3d4a953b9209054a93043b0c5cd1dfff01ad0df9
AsCraft <ascraft.contreras@outlook.cl>
2019-10-01 04:09:12 -0300

    Completacion  del lector de archivos
    El lector de archivos adquirio las funciones basicas para leer un archivo en una ruta relativa desde el pom.xlm.
    Faltaria el contructor y los metodos de seleccionador de ruta para archivos.

--- src/main/java/proyecto/atmosfera/controlador/LectorArchivo.java
+++ src/main/java/proyecto/atmosfera/controlador/LectorArchivo.java
@@ -1,49 +1,54 @@
 package proyecto.atmosfera.controlador;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class LectorArchivo {
 
 	private Path ruta;
-	private String AMANECER = "/Amanecer";
-	private String CENTRO = "/Centro";
-	private String EL_CARMEN = "/El Carmen";
-	private String LABRANZA = "/Labranza";
-	private String PEDRO_VALDIVIA = "/Pedro de Valdivia";
-	private String PONIENTE = "/Poniente";
-	private String PUEBLO_NUEVO = "/Pueblo Nuevo";
-	private String SANTA_ROSA = "/Santa Rosa";
-	private String UNIVERSIDAD = "/Universidad";
-	private String TEMUCO = "/Temuco";
-	private String ABRIL = "/Abril.csv";
-	private String AGOSTO = "/Agosto.csv";
-	private String JULIO = "/Julio.csv";
-	private String JUNIO = "/Junio.csv";
-	private String MAYO = "/Mayo.csv";
+	private final static String AMANECER = "/Amanecer";
+	private final static String CENTRO = "/Centro";
+	private final static String EL_CARMEN = "/El Carmen";
+	private final static String LABRANZA = "/Labranza";
+	private final static String PEDRO_VALDIVIA = "/Pedro de Valdivia";
+	private final static String PONIENTE = "/Poniente";
+	private final static String PUEBLO_NUEVO = "/Pueblo Nuevo";
+	private final static String SANTA_ROSA = "/Santa Rosa";
+	private final static String UNIVERSIDAD = "/Universidad";
+	private final static String TEMUCO = "/Temuco";
+	private final static String ABRIL = "/Abril.csv";
+	private final static String AGOSTO = "/Agosto.csv";
+	private final static String JULIO = "/Julio.csv";
+	private final static String JUNIO = "/Junio.csv";
+	private final static String MAYO = "/Mayo.csv";
 
 	public Path getRuta() {
 		return this.ruta;
 	}
 
-	/**
-	 * 
-	 * @param ruta
-	 */
 	public void setRuta(String ruta) {
-		// TODO - implement LectorArchivo.setRuta
-		throw new UnsupportedOperationException();
+		this.ruta = Paths.get(ruta);
 	}
 
-	public String[] leerArchivo() {
-		// TODO - implement LectorArchivo.leerArchivo
-		throw new UnsupportedOperationException();
+	public String[][] leerArchivo() throws IOException{
+		String datos = new String(Files.readAllBytes(ruta));
+		return separarDatos(datos);
 	}
 
 	/**
-	 * 
-	 * @param datos
+	 * Tomo un texto donde cada linea representa un  y la separa en los objetos que puede crear
+	 * y en los atributos de cada registro.
+	 * @param datos - Conjunto de atributos de registro.
 	 */
-	private void separarDatos(String datos) {
-		// TODO - implement LectorArchivo.separarDatos
-		throw new UnsupportedOperationException();
+	private String[][] separarDatos(String datos) {
+		String[] registros = datos.split("\n");
+		String[][] attriRegistros = new String[registros.length][4];
+		for (int i = 0; i < registros.length; i++) {
+			attriRegistros[i] = registros[i].split(",");
+		}
+		return attriRegistros;
 	}
 
 	public void selecMes() {
