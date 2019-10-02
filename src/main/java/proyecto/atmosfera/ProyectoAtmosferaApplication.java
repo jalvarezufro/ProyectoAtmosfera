6d592b84c5f52123e439862713cd2ae5ee250460
jalvarezufro <48539136+jalvarezufro@users.noreply.github.com>
2019-10-02 13:52:05 -0300

    version 2 Final.f

--- src/main/java/proyecto/atmosfera/ProyectoAtmosferaApplication.java
+++ src/main/java/proyecto/atmosfera/ProyectoAtmosferaApplication.java
@@ -15,15 +15,6 @@ public class ProyectoAtmosferaApplication {
     public static void main(String[] args) {
       SpringApplication.run(ProyectoAtmosferaApplication.class, args);
 
-        ManejoDato md = new ManejoDato();
-        ManejoRegistro rController = new ManejoRegistro();
-        ArrayList<Registro>  l = rController.registrosPorHora(md.leerArchivo("ProyectoAtmosfera.csv"));
-        String [][] arraymirar = l.get(1).contarValor(100,l,"30/04/2019","MP10");
-        for (int i = 0; i < arraymirar.length; i++) {
-            System.out.println(arraymirar[i][1]);
-        }
 
 
-    }
-
-}
+}}
