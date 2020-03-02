c6648d6578ac2a40cf6d18ec23b92fc8e4b4780e
Diego <diegolagosbeltran@gmail.com>
2020-03-02 00:29:54 -0300

    Refactorización de clases y archivos no utilizados

--- src/main/java/proyecto/atmosfera/ProyectoAtmosferaApplication.java
+++ src/main/java/proyecto/atmosfera/ProyectoAtmosferaApplication.java
@@ -2,13 +2,6 @@ package proyecto.atmosfera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import proyecto.atmosfera.controlador.ControlRegistro;
-import proyecto.atmosfera.manejoArchivos.ManejoDato;
-import proyecto.atmosfera.modelo.Registro;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class ProyectoAtmosferaApplication {
